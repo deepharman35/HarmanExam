cb803cb3cad8243a4be00662748ae81a7e80cce6
DEPP47 <harmanpawar35@gmail.com>
2024-02-22 09:45:58 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { HeaderComponent } from './header/header.component';
+import { OutputComponent } from './output/output.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    HeaderComponent,
+    OutputComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
