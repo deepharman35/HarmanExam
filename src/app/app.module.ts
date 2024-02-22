f8b775b01a835de3a18ede746c07c6b607b6631f
DEPP47 <harmanpawar35@gmail.com>
2024-02-22 09:52:09 -0500

    coded some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { HeaderComponent } from './header/header.component';
 import { OutputComponent } from './output/output.component';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,10 +14,12 @@ import { FooterComponent } from './footer/footer.component';
     InputComponent,
     HeaderComponent,
     OutputComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TeacherDataService } from '../teacher-data.service';
 
 @Component({
   selector: 'app-input',
-  templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  templateUrl: './input.component.html'
 })
 export class InputComponent {
+  firstName = '';
+  lastName = '';
+  employmentTerm = 'Full-time'; // Default selection
 
+  constructor(private teacherDataService: TeacherDataService, private router: Router) {}
+
+  submit() {
+    this.teacherDataService.setTeacherData({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      employmentTerm: this.employmentTerm
+    });
+    this.router.navigate(['/output']);
+  }
 }
