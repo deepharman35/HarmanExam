0a75054dbcb11947ee27b8072b09cade5fa2f31c
DEPP47 <harmanpawar35@gmail.com>
2024-02-22 09:56:07 -0500

    added code for html

--- src/app/teacher-data.service.ts
+++ src/app/teacher-data.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TeacherDataService {
-  private teacherData = {};
+  private teacherData = { firstName: '', lastName: '', employmentTerm: '' };
 
   setTeacherData(data: any) {
     this.teacherData = data;
